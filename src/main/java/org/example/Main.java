f7cd93341cdaab8fc4e067981451cebf2ec45e50
doguhannilt <doguhannilt@gmail.com>
2025-02-12 16:24:22 +0300

    Lesson 3 - Completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,36 @@ package org.example;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 import org.example.entities.Employee;
-import org.example.entities.Product;
 import org.example.persistence.CustomPersistenceUnitInfo;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+
+        Map<String, String> props = new HashMap<>();
+
+        // It's a special feature Hibernate has. You can see details about the SQL
+        // process and its mutation that JPA does when you run the project.
+        // Example: Hibernate: select e1_0.id,e1_0.address,e1_0.name from Employee e1_0 where e1_0.id=?
+        props.put("hibernate.show_sql","true");
+
+        /*
+        * hibernate.hbm2ddl.auto Automatically validates or exports schema DDL
+        * to the database when the SessionFactory is created. With create-drop,
+        * the database schema will be dropped when the SessionFactory is closed explicitly.
+            validate: validate the schema, makes no changes to the database.
+            create-only: database creation will be generated.
+            drop: database dropping will be generated.
+            update: update the schema.
+            create: creates the schema, destroying previous data.
+            create-drop: drop the schema when the SessionFactory is closed explicitly, typically when the application is stopped.
+            none: does nothing with the schema, makes no changes to the database
+        */
+        props.put("hibernate.hbm2ddl.auto", "none");
 
         // The following code (commented out) shows an alternate method where you would use the default
         // "persistence.xml" configuration file instead of programmatically creating the EntityManagerFactory.
@@ -23,7 +43,7 @@ public class Main {
         // CustomPersistenceUnitInfo holds specific configurations like database connection settings, entities, etc.
         // It is a more dynamic and flexible approach to configuration compared to XML-based configurations.
         EntityManagerFactory entityManagerFactory = new HibernatePersistenceProvider()
-                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(), new HashMap<>());
+                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(),props);
 
         // Creating the EntityManager instance here. The EntityManager is the main interface for interacting with
         // the persistence context, which means managing entities, executing queries, and performing CRUD operations.
@@ -41,31 +61,40 @@ public class Main {
             // - Create a new Employee object
             // - Set the employee's properties (address, name, id)
             // - Persist it to the database using entityManager.persist() to save the entity.
-            //
-            //             Employee employee = new Employee();
-            //             employee.setAddress("Example address 2");
-            //             employee.setName("Example name 2");
-            //             employee.setId(2);
-            //             entityManager.persist(employee);
 
+                         // Employee employee = new Employee();
+                         // employee.setAddress("Example address 2");
+                         // employee.setName("Example name 2");
+                         // entityManager.persist(employee);
 
             // Instead of adding a new employee, here we're retrieving an existing employee from the database.
             // The 'find' method is used to look up the Employee by its primary key (id).
             // In this case, we're finding the employee with an ID of 1, and then printing their name to the console.
             // The 'find' method retrieves the object from the database if it exists or returns null if not found.
-            Employee employeeProp = entityManager.find(Employee.class, 1);
-            System.out.println(employeeProp.getName()); // Output the name of the employee to the console.
-
+            // Employee employeeProp = entityManager.find(Employee.class, 1);
+            // System.out.println(employeeProp.getName()); // Output the name of the employee to the console.
 
 
             // Renaming the employee name
-            employeeProp.setName("Doguhan");
-            System.out.println(employeeProp.getName()); // Output the name of the employee to the console.
-
+            // employeeProp.setName("Doguhan");
+            // System.out.println(employeeProp.getName());  Output the name of the employee to the console.
 
             // Delete an employee
-            // Employee employeeProp2 = entityManager.find(Employee.class, 2);
             // entityManager.remove(employeeProp2);
+
+            /*
+            * The find method always returns an entity object.
+            * Hibernate initializes its attributes based on the defined FetchTypes.
+            * If you're using a default mapping, Hibernate fetches all basic attributes and initializes all to-one associations.
+            * The getReference method returns a reference to an entity object.
+            */
+            Employee employeeProp2 = entityManager.find(Employee.class, 2);
+            Employee reference = entityManager.getReference(Employee.class, 2);
+            System.out.println(employeeProp2);
+            System.out.println(reference);
+
+            // Reload all the data.
+            entityManager.refresh(employeeProp2);
 
 
             // Commit the transaction. This is where all the changes (if any) are flushed to the database.
@@ -76,14 +105,11 @@ public class Main {
 
             // Nothing has been changed because our value has been stored in the context but hasn't been
             // committed to the database.
-            employeeProp.setName("Renaming the employee without commit the transaction");
-            System.out.println(employeeProp.getName()); // Output the name of the employee to the console.
-
+            // employeeProp.setName("Set a new name to the employee without commit the transaction");
         } finally {
             // Ensure that the EntityManager is closed, releasing resources such as database connections.
             // This is important to avoid memory leaks and to properly manage the connection pool.
             entityManager.close();
         }
-
     }
 }
--- src/main/java/org/example/entities/Employee.java
+++ src/main/java/org/example/entities/Employee.java
@@ -1,10 +1,7 @@
 package org.example.entities;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 // @Entity(name="") <--  JPA specifies the entity and how it should be named.
 @Entity
@@ -13,6 +10,13 @@ import jakarta.persistence.Table;
 public class Employee {
 
     @Id
+    /*
+    GenerationType.IDENTITY: This strategy will help us to generate the primary key value by the database itself using the auto-increment column option. It relies on the database’s native support for generating unique values.
+    GenerationType.AUTO: This is a default strategy and the persistence provider which automatically selects an appropriate generation strategy based on the database usage.
+    GenerationType.TABLE: This strategy uses a separate database table to generate primary key values. The persistence provider manages this table and uses it to allocate unique values for primary keys.
+    GenerationType.SEQUENCE: This generation-type strategy uses a database sequence to generate primary key values. It requires the usage of database sequence objects, which varies depending on the database which is being used.
+    */
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="id") // Specify the column name, or your variable name is going to be the column name.
     private int id;
 
